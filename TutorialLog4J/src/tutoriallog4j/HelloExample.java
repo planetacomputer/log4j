da0ce12b52f618777b1f2a4686ce9102931ff388
planetacomputer <evapardeza123@gmail.com>
2016-04-27 13:12:08 +0200

    Primer commit

---
+++ TutorialLog4J/src/tutoriallog4j/HelloExample.java
@@ -0,0 +1,39 @@
+package tutoriallog4j;
+
+import org.apache.log4j.Logger;
+
+public class HelloExample{
+	
+	final static Logger logger = Logger.getLogger(HelloExample.class);
+	
+	public static void main(String[] args) {
+	
+		HelloExample obj = new HelloExample();
+		obj.runMe("mkyong");
+                obj.metodo2();
+		
+	}
+	
+	private void runMe(String parameter){
+		
+		if(logger.isDebugEnabled()){
+			logger.debug("This is debug : " + parameter);
+		}
+		
+		if(logger.isInfoEnabled()){
+			logger.info("This is info : " + parameter);
+		}
+		
+		logger.warn("This is warn : " + parameter);
+		logger.error("This is error : " + parameter);
+		logger.fatal("This is fatal : " + parameter);
+		
+	}
+        
+        private void metodo2(){
+            if(logger.isInfoEnabled()){
+			logger.info("Este es un debug desde metodo 2 ");
+		}
+        }
+	
+}
---
+++ TutorialLog4J/src/tutoriallog4j/TutorialLog4J.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutoriallog4j;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Ocupacional 2016-04
+ */
+public class TutorialLog4J {
+
+    final static Logger logger = Logger.getLogger(TutorialLog4J.class);
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
